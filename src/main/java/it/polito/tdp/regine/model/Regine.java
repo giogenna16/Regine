ea24112ab70047ecfbb2d607f5a89b338ea35f51
genna <genna@LAPTOP-HDR1FFT9.station>
2021-04-03 16:32:11 +0200

    soluzione mia

---
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -0,0 +1,54 @@
+package it.polito.tdp.regine.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Regine {
+
+	
+	// N è il numero di righe e colonne della scacchiera
+	//   (righe e colonne numerate da 0 a N-1)
+	// ad ogni livello posizioniamo una regina in una nuova riga
+	
+	// soluzione parziale: lista delle colonne in cui mettere le regine (prime righe)
+	// 		List<Integer>
+	// livello = quante righe sono già piene
+	// livello = 0 => nessuna riga piena (devo mettere la regina nella riga 0)
+	// livello = 3 => 3 righe piene (0, 1, 2), devo mettere la regina nella riga 3
+	// [0]
+	//     [0, 2]
+	//            [0, 2, 1]
+	public void posizioni(int N){
+		List<Integer> parziale= new LinkedList<>();
+		parziale.add(0);
+		cerca(parziale, 1, N);
+	}
+	
+	private void cerca(List<Integer>parziale, int livello, int N) {
+		if(livello==N) {
+			System.out.println(parziale);
+			// caso terminale
+		} else {
+			
+			for(int colonna=0; colonna<N; colonna++) {
+				int n=0;
+				for(int i=1; i<=livello; i++) {
+				if(Math.abs(parziale.get(livello-i)-colonna)!=i && !parziale.contains(colonna)) {
+					n++;
+				}
+				
+				}
+				if(n==livello) {
+				List<Integer> nuovaParziale= new LinkedList<>(parziale);
+				nuovaParziale.add(colonna);
+				cerca(nuovaParziale, livello+1, N);	
+				}
+				// if la possa nella casella [livello][colonna] è valida
+				// se sì, aggiungi a parziale e fai ricorsione
+			}
+		}
+	}
+	
+	
+	
+}
---
+++ src/main/java/it/polito/tdp/regine/model/TestModel.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.regine.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		Regine prova= new Regine();
+		prova.posizioni(10);
+	}
+
+}
